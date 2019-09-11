db77670b8a79208ba810c2cef393878b8b43fb29
Vincent Therrien <vincent.therrien@polymtl.ca>
2019-09-11 00:08:53 -0400

    Implemented methods for billet and gestionnaire. Need to add comments and fix two methods.

--- TP1/TP1/billet.cpp
+++ TP1/TP1/billet.cpp
@@ -4,8 +4,100 @@
 
 Billet::Billet()
 {
+	pnr_ = "";
+	nomPassager_ = "";
+	prix_ = 0.0;
+	od_ = "";
+	tarif_ = TarifBillet::Economie;
+	dateVol_ = "";
 }
 
+Billet::Billet(const string& pnr, const string& nomPassager,
+			   double prix, const string& od, TarifBillet tarif,
+			   const string& dateVol)
+{
+	pnr_ = pnr;
+	nomPassager_ = nomPassager;
+	prix_ = prix;
+	od_ = od;
+	tarif_ = tarif;
+	dateVol_ = dateVol;
+}
+
+string Billet::getPnr()
+{
+	return pnr_;
+}
+string Billet::getNomPassager()
+{
+	return nomPassager_;
+}
+double Billet::getPrix()
+{
+	return prix_;
+}
+string Billet::getOd()
+{
+	return od_;
+}
+TarifBillet Billet::getTarif()
+{
+	return tarif_;
+}
+string Billet::getDateVol()
+{
+	return dateVol_;
+}
+
+void Billet::setPnr(const string& pnr)
+{
+	pnr_ = pnr;
+}
+void Billet::setNomPassager(const string& nomPassager)
+{
+	nomPassager_ = nomPassager;
+}
+void Billet::setPrix(double prix)
+{
+	prix_ = prix;
+}
+void Billet::setOd(const string& od)
+{
+	od_ = od;
+}
+void Billet::setTarif(TarifBillet tarif)
+{
+	tarif_ = tarif;
+}
+void Billet::setDateVol(const string& dateVol)
+{
+	dateVol_ = dateVol;
+}
+
+string Billet::formatTarif(TarifBillet tarif)
+{
+	switch (tarif) {
+	case TarifBillet::Economie:
+		return "Economie";
+	case TarifBillet::PremiumEconomie:
+		return "PremiumEconomie";
+	case TarifBillet::Affaire:
+		return "Affaire";
+	case TarifBillet::Premiere:
+		return "Premiere";
+	}
+	return "Invalid formatTarif";
+}
+
+void Billet::afficherBillet()
+{
+	cout << "\t\t - Billet " << pnr_ << "(Classe : "
+		<< formatTarif(tarif_) << endl;
+	cout << "\t\t\t - Passager \t : " << nomPassager_;
+	cout << "\t\t\t - Prix \t : " << prix_;
+	cout << "\t\t\t - Trajet \t : " << od_;
+	cout << "\t\t\t - Vol le \t : " << dateVol_;
+}
 
 Billet::~Billet()
 {
--- TP1/TP1/gestionnaire.cpp
+++ TP1/TP1/gestionnaire.cpp
@@ -4,8 +4,146 @@
 
 Gestionnaire::Gestionnaire()
 {
+	membres_ = new Membre*[];
+	nbMembres_ = 0;
+	capaciteMembres_ = CAPACITE_INITIALE;
+	coupons_ = new Coupon*[];
+	nbCoupons_ = 0;
+	capaciteCoupons_ = CAPACITE_INITIALE;
 }
 
+Membre** Gestionnaire::getMembres()
+{
+	return membres_;
+}
+
+int Gestionnaire::getNbMembres()
+{
+	return nbMembres_;
+}
+
+int Gestionnaire::getCapaciteMembres()
+{
+	return capaciteCoupons_;
+}
+
+Coupon** Gestionnaire::getCoupons()
+{
+	return coupons_;
+}
+
+int Gestionnaire::getNbCoupons()
+{
+	return nbCoupons_;
+}
+
+int Gestionnaire::getCapaciteCoupons()
+{
+	return capaciteCoupons_;
+}
+
+void Gestionnaire::ajouterMembre(const string& nomMembre)
+{
+	// Increase the size of the container if need be.
+	if (nbMembres_ >= capaciteMembres_) {
+		Membre** expandedArray = new Membre*[capaciteMembres_*2];
+		for (unsigned int i = 0; i < capaciteMembres_; i++) {
+			expandedArray[i] = membres_[i];
+		}
+		delete[] membres_;
+		membres_ = expandedArray;
+		capaciteMembres_ *= 2;
+	}
+	membres_[nbMembres_] = new Membre();
+	nbMembres_++;
+}
+
+void Gestionnaire::ajouterCoupon(const string& code, double rabais, int cout)
+{
+	if (nbCoupons_ >= capaciteCoupons_) {
+		Coupon** expandedArray = new Coupon*[capaciteCoupons_ * 2];
+		for (unsigned int i = 0; i < capaciteCoupons_; i++) {
+			expandedArray[i] = coupons_[i];
+		}
+		delete[] coupons_;
+		coupons_ = expandedArray;
+		capaciteCoupons_ *= 2;
+	}
+	coupons_[nbCoupons_] = new Coupon(code, rabais, cout);
+	nbCoupons_++;
+}
+
+Membre* Gestionnaire::trouverMembre(const string& nomMembre)
+{
+	for (int i = 0; i < nbMembres_; i++) {
+		if (membres_[i]->getNom() == nomMembre) {
+			return membres_[i];
+		}
+	}
+	cout << "Member '" << nomMembre << "' was not found." << endl;
+	return nullptr;
+}
+
+void Gestionnaire::assignerBillet(const string& nomMembre,
+	const string& pnr, double prixBase, const string& od,
+	TarifBillet tarif, const string& dateVol,
+	bool utiliserCoupon)
+{
+	double prix = prixBase;
+	if (utiliserCoupon) {
+		// TODO
+	}
+	trouverMembre(nomMembre)->ajouterBillet(pnr, prix, od,
+		tarif, dateVol);
+}
+
+double Gestionnaire::appliquerCoupon(Membre* membre, double prix)
+{
+	Coupon** coupons = membre->getCoupons();
+	int nCoupons = membre->getNbCoupons();
+	if (nCoupons == 0) {
+		cout << "Member " << membre->getNom() << " has "
+			<< "no coupon." << endl;
+		return;
+	}
+	Coupon* bestCoupon = coupons[0];
+	for (int i = 1; i < nCoupons; i++) {
+		double rabais = coupons[i]->getRabais();
+		if (rabais > bestCoupon->getRabais()) {
+			bestCoupon = coupons[i];
+		}
+	}
+	double discoutOnPrice = bestCoupon->getRabais() * prix;
+	membre->retirerCoupon(bestCoupon);
+	return discoutOnPrice;
+}
+
+void Gestionnaire::acheterCoupon(const string& nomMembre)
+{
+	Coupon* bestCoupon = coupons_[0];
+	for (int i = 1; i < nbCoupons_; i++) {
+		double rabais = coupons_[i]->getRabais();
+		if (rabais > bestCoupon->getRabais()) {
+			bestCoupon = coupons_[i];
+		}
+	}
+	Membre* membre = trouverMembre(nomMembre);
+	if (membre->getPoints >= bestCoupon->getCout) {
+		trouverMembre(nomMembre)->acheterCoupon(bestCoupon);
+	}
+	else {
+		cout << "Member '" << nomMembre << "' cannot "
+			<< "buy a coupon." << endl;
+	}
+	// TODO: pop the coupon
+}
+
+void Gestionnaire::afficherInfos()
+{
+	for (int i = 0; i < nbMembres_; i++) {
+		membres_[i]->afficherMembre();
+	}
+}
 
 Gestionnaire::~Gestionnaire()
 {
