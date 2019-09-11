218ace11deebd41ace0548b126576e953d67cf29
Vincent Therrien <vincent.therrien@polymtl.ca>
2019-09-11 14:36:44 -0400

    Documented all methods except those whose content is not implement (in class Gestionnaire). Class Billet is basically completed.

--- TP1/TP1/billet.cpp
+++ TP1/TP1/billet.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "billet.h"
 
-
+/**
+*@brief Billet default constructor. Initializes values to 0 or empty strings.
+*/
 Billet::Billet()
 {
 	pnr_ = "";
@@ -12,6 +14,15 @@ Billet::Billet()
 	dateVol_ = "";
 }
 
+/**
+*@brief Billet constructor by parameter.
+*@param pnr Ticket reservation number (used as an ID).
+*@param nomPassager Passenger name.
+*@param prix Ticket price.
+*@param od origin and destination.
+*@param tarif Price reduction.
+*@param dateVol Flight date.
+*/
 Billet::Billet(const string& pnr, const string& nomPassager,
 			   double prix, const string& od, TarifBillet tarif,
 			   const string& dateVol)
@@ -24,56 +35,119 @@ Billet::Billet(const string& pnr, const string& nomPassager,
 	dateVol_ = dateVol;
 }
 
+/**
+*@brief Getter for attribute pnr.
+*@return attribute pnr_.
+*/
 string Billet::getPnr()
 {
 	return pnr_;
 }
+
+/**
+*@brief Getter for attribute nomPassager.
+*@return attribute nomPassager_.
+*/
 string Billet::getNomPassager()
 {
 	return nomPassager_;
 }
+
+/**
+*@brief Getter for attribute prix.
+*@return attribute prix_.
+*/
 double Billet::getPrix()
 {
 	return prix_;
 }
+
+/**
+*@brief Getter for attribute od.
+*@return attribute od_.
+*/
 string Billet::getOd()
 {
 	return od_;
 }
+
+/**
+*@brief Getter for attribute tarif.
+*@return attribute tarif_.
+*/
 TarifBillet Billet::getTarif()
 {
 	return tarif_;
 }
+
+/**
+*@brief Getter for attribute dateVol.
+*@return attribute dateVol_.
+*/
 string Billet::getDateVol()
 {
 	return dateVol_;
 }
 
+/**
+*@brief Setter for attribute pnr.
+*@param pnr new reseration number.
+*/
 void Billet::setPnr(const string& pnr)
 {
 	pnr_ = pnr;
 }
+
+/**
+*@brief Setter for attribute nomPassager.
+*@param nomPassager new passenger name.
+*/
 void Billet::setNomPassager(const string& nomPassager)
 {
 	nomPassager_ = nomPassager;
 }
+
+/**
+*@brief Setter for attribute prix.
+*@param prix new price.
+*/
 void Billet::setPrix(double prix)
 {
 	prix_ = prix;
 }
+
+/**
+*@brief Setter for attribute od.
+*@param od new origin and destination.
+*/
 void Billet::setOd(const string& od)
 {
 	od_ = od;
 }
+
+/**
+*@brief Setter for attribute tarif.
+*@param tarif new price reduction.
+*/
 void Billet::setTarif(TarifBillet tarif)
 {
 	tarif_ = tarif;
 }
+
+/**
+*@brief Setter for attribute dateVol.
+*@param dateVol new flight date.
+*/
 void Billet::setDateVol(const string& dateVol)
 {
 	dateVol_ = dateVol;
 }
 
+/**
+*@brief Format price reduction as a string instead of an enumeration.
+*@param tarif Price reduction (as an enumeration).
+*@return A character string for the price reduction.
+*/
 string Billet::formatTarif(TarifBillet tarif)
 {
 	switch (tarif) {
@@ -89,6 +163,9 @@ string Billet::formatTarif(TarifBillet tarif)
 	return "Invalid formatTarif";
 }
 
+/**
+*@brief Print all ticket attributes with an indentation.
+*/
 void Billet::afficherBillet()
 {
 	cout << "\t\t - Billet " << pnr_ << "(Classe : "
@@ -99,6 +176,7 @@ void Billet::afficherBillet()
 	cout << "\t\t\t - Vol le \t : " << dateVol_;
 }
 
+// TODO: check if the destructor is really necessary.
 Billet::~Billet()
 {
 }
--- TP1/TP1/gestionnaire.cpp
+++ TP1/TP1/gestionnaire.cpp
@@ -1,47 +1,78 @@
 #include "pch.h"
 #include "gestionnaire.h"
 
-
+/**
+*@brief Gestionnaire default constructor. Initializes empty arrays and
+*       attributes.
+*/
 Gestionnaire::Gestionnaire()
 {
-	membres_ = new Membre*[];
+	membres_ = new Membre*[0];
 	nbMembres_ = 0;
 	capaciteMembres_ = CAPACITE_INITIALE;
-	coupons_ = new Coupon*[];
+	coupons_ = new Coupon*[0];
 	nbCoupons_ = 0;
 	capaciteCoupons_ = CAPACITE_INITIALE;
 }
 
+/**
+*@brief Get array 'membres_'.
+*@return attribute membres_.
+*/
 Membre** Gestionnaire::getMembres()
 {
 	return membres_;
 }
 
+/**
+*@brief Get the number of members.
+*@return attribute nbMembres_.
+*/
 int Gestionnaire::getNbMembres()
 {
 	return nbMembres_;
 }
 
+/**
+*@brief Get the total size of array 'membres_'.
+*@return attribute capaciteMembres_.
+*/
 int Gestionnaire::getCapaciteMembres()
 {
-	return capaciteCoupons_;
+	return capaciteMembres_;
 }
 
+/**
+*@brief Get the array of coupons.
+*@return attribute coupons_.
+*/
 Coupon** Gestionnaire::getCoupons()
 {
 	return coupons_;
 }
 
+/**
+*@brief Get the number of coupons.
+*@return attribute nbCoupons_.
+*/
 int Gestionnaire::getNbCoupons()
 {
 	return nbCoupons_;
 }
 
+/**
+*@brief Get the total size of array 'coupons_'.
+*@return attribute capaciteCoupons_.
+*/
 int Gestionnaire::getCapaciteCoupons()
 {
 	return capaciteCoupons_;
 }
 
+/**
+*@brief Create a new object 'Membre' and add it to the array of members.
+*@param nomMembre The new member's name.
+*/
 void Gestionnaire::ajouterMembre(const string& nomMembre)
 {
 	// Increase the size of the container if need be.
@@ -58,6 +89,12 @@ void Gestionnaire::ajouterMembre(const string& nomMembre)
 	nbMembres_++;
 }
 
+/**
+*@brief Create a new object 'Coupon' and add it to the array of coupons.
+*@param code The new coupon's code.
+*@param rabais Price reduction for this coupon.
+*@param cout Cost of the new coupon.
+*/
 void Gestionnaire::ajouterCoupon(const string& code, double rabais, int cout)
 {
 	if (nbCoupons_ >= capaciteCoupons_) {
@@ -73,6 +110,11 @@ void Gestionnaire::ajouterCoupon(const string& code, double rabais, int cout)
 	nbCoupons_++;
 }
 
+/**
+*@brief Find and return a member in the list of members.
+*@param nomMembre Name of the member to look for.
+*@return Pointer to the member of the desired name.
+*/
 Membre* Gestionnaire::trouverMembre(const string& nomMembre)
 {
 	for (int i = 0; i < nbMembres_; i++) {
@@ -84,6 +126,9 @@ Membre* Gestionnaire::trouverMembre(const string& nomMembre)
 	return nullptr;
 }
 
+/**
+*@brief TODO
+*/
 void Gestionnaire::assignerBillet(const string& nomMembre,
 	const string& pnr, double prixBase, const string& od,
 	TarifBillet tarif, const string& dateVol,
@@ -97,6 +142,9 @@ void Gestionnaire::assignerBillet(const string& nomMembre,
 		tarif, dateVol);
 }
 
+/**
+*@brief TODO
+*/
 double Gestionnaire::appliquerCoupon(Membre* membre, double prix)
 {
 	Coupon** coupons = membre->getCoupons();
@@ -104,7 +152,7 @@ double Gestionnaire::appliquerCoupon(Membre* membre, double prix)
 	if (nCoupons == 0) {
 		cout << "Member " << membre->getNom() << " has "
 			<< "no coupon." << endl;
-		return;
+		return 0.0;
 	}
 	Coupon* bestCoupon = coupons[0];
 	for (int i = 1; i < nCoupons; i++) {
@@ -118,6 +166,9 @@ double Gestionnaire::appliquerCoupon(Membre* membre, double prix)
 	return discoutOnPrice;
 }
 
+/**
+*@brief TODO
+*/
 void Gestionnaire::acheterCoupon(const string& nomMembre)
 {
 	Coupon* bestCoupon = coupons_[0];
@@ -128,7 +179,7 @@ void Gestionnaire::acheterCoupon(const string& nomMembre)
 		}
 	}
 	Membre* membre = trouverMembre(nomMembre);
-	if (membre->getPoints >= bestCoupon->getCout) {
+	if (membre->getPoints() >= bestCoupon->getCout()) {
 		trouverMembre(nomMembre)->acheterCoupon(bestCoupon);
 	}
 	else {
@@ -138,6 +189,9 @@ void Gestionnaire::acheterCoupon(const string& nomMembre)
 	// TODO: pop the coupon
 }
 
+/**
+*@brief Print information for all members.
+*/
 void Gestionnaire::afficherInfos()
 {
 	for (int i = 0; i < nbMembres_; i++) {
@@ -145,6 +199,17 @@ void Gestionnaire::afficherInfos()
 	}
 }
 
+/**
+*@brief Delete attributes 'membres_' and 'coupons_' and their content.
+*/
 Gestionnaire::~Gestionnaire()
 {
+	for (int i = 0; i < nbMembres_; i++) {
+		delete membres_[i];
+	}
+	delete[] membres_;
+	for (int i = 0; i < nbCoupons_; i++) {
+		delete coupons_[i];
+	}
+	delete[] coupons_;
 }
