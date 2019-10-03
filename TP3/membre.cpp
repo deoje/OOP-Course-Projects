fa3acdbb0f7addbba3309de8e2bcbe0f07caa0dd
Derek <derek.ojeda-centeno@polymtl.ca>
2019-10-03 19:39:35 -0400

    Fixed the way we print the member by verifying the type of the Billet instead of dynamic_cast

--- TP3/membre.cpp
+++ TP3/membre.cpp
@@ -161,7 +161,7 @@ ostream& operator<<(ostream& o, const Membre& membre)
 	o << "- Membre " << membre.nom_ << ":" << endl;
 	o << "\t" << "- Billets :" << endl;
 	for (int i = 0; i < membre.billets_.size(); i++) {
-		if (dynamic_cast<FlightPass*>(membre.billets_[i])) {
+		if (membre.billets_[i]->getTypeBillet() == Flight_Pass) {
 			o << *(static_cast<FlightPass*>(membre.billets_[i]));
 			continue;
 		}
